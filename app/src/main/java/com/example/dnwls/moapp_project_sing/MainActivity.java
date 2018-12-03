48ad8ff049d6599379f362e1d896bed11e0bf048
dnwls <dnwlscjf167@naver.com>
2018-12-04 01:45:44 +0900

    start

---
+++ app/src/main/java/com/example/dnwls/moapp_project_sing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dnwls.moapp_project_sing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
